ce571b19f5cbb5e54d34d9d55789baabe5ba2181
Дюков-Франци Александр Григорьевич <dyukov-ag@ranepa.ru>
2019-03-13 17:42:04 +0300

    Added package nodemon and FL parser

--- server.js
+++ server.js
@@ -3,6 +3,39 @@ const app = express();
 const axios = require('axios');
 const cheerio = require('cheerio');
 
+app.get('/api/getMenu', (req, res) => {
+  res.set('Content-Type', 'application/json; charset: utf-8');
+  res.end(JSON.stringify([
+    {
+      id: '/',
+      text: "Главная",
+      active: true
+    },
+    {
+      id: '/portfolio',
+      text: "Портфолио",
+      active: false
+    },
+    {
+      id: '/contacts',
+      text: "Контакты",
+      active: false
+    }
+  ]));
+});
+
+app.get('/api/getPortfolioFL', (req, res) => {
+  res.set('Content-Type', 'application/json; charset: utf-8');
+  getPortfolioFL(null, result => res.end(JSON.stringify(result)));
+});
+
+app.get('/api/getPortfolioGit', (req, res) => {
+  res.set('Content-Type', 'application/json; charset: utf-8');
+  getPortfolioGit(null, result => res.end(JSON.stringify(result)));
+});
+
+app.listen(3001, console.log('Сервер работает на порту 3001'));
+
 
 function getPortfolioFL(err, cb) {
 
@@ -43,61 +76,42 @@ function getPortfolioFL(err, cb) {
     })
     .catch(err => console.error(err.message));
 }
+function getPortfolioGit(err, cb) {
 
-app.get('/api/getMenu', (req, res) => {
-  res.set('Content-Type', 'application/json; charset: utf-8');
-  res.end(JSON.stringify([
-    {
-      id: '/',
-      text: "Главная",
-      active: true
-    },
-    {
-      id: '/portfolio',
-      text: "Портфолио",
-      active: false
-    },
-    {
-      id: '/contacts',
-      text: "Контакты",
-      active: false
-    }
-  ]));
-});
+  const result = [];
+  let countPage = 0;
 
-app.get('/api/getPortfolioFL', (req, res) => {
-  res.set('Content-Type', 'application/json; charset: utf-8');
-  getPortfolioFL(null, result => res.end(JSON.stringify(result)));
-});
+  axios({
+      url: 'https://github.com/AlexDF89/',
+      method: 'GET'
+    })
+    .then(async (response) => {
 
-app.get('/api/getPortfolioGit', (req, res) => {
-  res.set('Content-Type', 'application/json; charset: utf-8');
-  res.end(JSON.stringify([
-    {
-      type: ['Github'],
-      title: 'Портфолио Github 1',
-      src: '',
-      desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi at rem omnis odio ad vero amet delectus. Sequi culpa deserunt commodi eius corporis vero placeat consequatur facere tempore, necessitatibus minus cupiditate expedita vel, suscipit dolorum officiis temporibus quaerat non?'
-    },
-    {
-      type: ['Games', 'Github'],
-      title: 'Портфолио Games 1',
-      src: '',
-      desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta harum ipsa temporibus dolorem odit quis velit vero at, distinctio aut. Deserunt hic animi saepe sunt at reiciendis id voluptate officiis in tempora nobis vel qui nihil, excepturi facilis obcaecati praesentium unde est sint temporibus fuga atque recusandae? Vero, non ratione!'
-    },
-    {
-      type: ['Github'],
-      title: 'Портфолио Github 2',
-      src: '',
-      desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit repellat, eos qui maiores esse repudiandae ut dicta! Recusandae nobis maiores aut quia possimus, obcaecati iure quae labore accusamus blanditiis! Explicabo ad possimus excepturi in error, iste aliquam ab eligendi quod hic? Harum numquam consequuntur officia dolor quia ipsam culpa nemo aspernatur. Dolore incidunt ipsum sit nesciunt ratione? Iure neque atque earum vel adipisci eligendi ipsum? Neque qui quibusdam vel adipisci!'
-    },
-    {
-      type: ['Games', 'Github'],
-      title: 'Портфолио Games 2',
-      src: '',
-      desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit repellat, eos qui maiores esse repudiandae ut dicta! Recusandae nobis maiores aut quia possimus, obcaecati iure quae labore accusamus blanditiis! Explicabo ad possimus excepturi in error, iste aliquam ab eligendi quod hic? Harum numquam consequuntur officia dolor quia ipsam culpa nemo aspernatur.'
-    }
-  ]));
-});
+      const $ = cheerio.load(response.data);
+      const lengthFL = $('.pinned-item-list-item-content a').length;
 
-app.listen(3001, console.log('Сервер работает на порту 3001'));
+      await $('.pinned-item-list-item-content a').each( function() {
+        axios(({
+          url: `https://github.com${this.attribs.href}`,
+          method: 'GET'
+        }))
+        .then(response => {
+          const $ = cheerio.load(response.data);
+          result[countPage] = {
+            type: ['Github'],
+            title: $('h1 strong a').text(),
+            src: '',
+            desc: $('.markdown-body.entry-content p:first-child').text().trim(),
+            href: `https://github.com${this.attribs.href}`
+          };
+          countPage++;
+
+          if (countPage === lengthFL) cb(result);
+
+        })
+        .catch(err => console.error(err.message));
+      });
+
+    })
+    .catch(err => console.error(err.message));
+}
