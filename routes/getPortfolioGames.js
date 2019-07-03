d24d141bfc18f0d67217c9c3ace69e1885fe3a98
Дюков-Франци Александр Григорьевич <dyukov-ag@ranepa.ru>
2019-07-03 17:08:44 +0300

    Добавлено описание проекта

--- routes/getPortfolioGames.js
+++ routes/getPortfolioGames.js
@@ -6,15 +6,18 @@ function getPortfolioGit(req, res) {
         type: ['Games'],
         title: 'Memoji_game',
         src: './images/portfolio/memoji.png',
-        desc: 'Игра memoji_game (напиши уже описание)',
-        href: 'http://localhost:3002'
+        desc: `Игра memoji_game изначально создавалась в качестве финального проекта для прохождения специализации от Яндекса и МФТИ "Разработка интерфейсов: вёрстка и JavaScript" на [coursera.org](https://www.coursera.org/specializations/razrabotka-interfeysov). Первый вариант игры был сделан на чистом html, css и JavaScript без использования библиотек и фреймворков. Игра работала исключительно на клиенте, без какого-либо бэкенда.
+        В дальнейшем логика была перенесена на node.js, а frontend на react.js.`,
+        href: 'http://memoji.webdev-master.ru'
       },
       {
         type: ['Games'],
         title: 'Checkers',
         src: './images/portfolio/shashki.jpg',
-        desc: 'Игра checkers (напиши уже описание)',
-        href: 'http://localhost:3001'
+        desc: `Шашки – это настольная игра для двух участников, в которую играют на специально расчерченном поле (доске) с помощью разноцветных фишек (шашек).<br>
+        Цель игры состоит в том, чтобы захватить все шашки соперника или в том, чтобы лишить их права передвижения.<br>
+        Запустив проект на рабочем сервере, вы сможете играть в шашки с друзьями по сети.`,
+        href: 'http://checkers.webdev-master.ru'
       }
     ]
   ));
